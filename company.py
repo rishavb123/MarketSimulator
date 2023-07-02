f58d4eb1b84bd0d363acd0e715755053956283b2
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-07-02 18:13:52 -0400

    added company entity

---
+++ company.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from entity import Entity
+
+class Company(Entity):
+
+    def __init__(self, symbol, noise_generator):
+        super().__init__()
+        self.symbol = symbol
+        self.noise_generator = noise_generator
+        self.num_shares = 0
+
+    def release_shares(self, num_shares_to_release):
+        self.num_shares += num_shares_to_release
+        self.update_holdings(self.symbol, num_shares_to_release)
+
+    def get_total_value(self):
+        return self.noise_generator.get_value()
+
+    def price_per_share(self):
+        assert self.num_shares > 0
+        return self.get_total_value() / self.num_shares
+    
+    def tick(self):
+        return self.noise_generator.tick()
