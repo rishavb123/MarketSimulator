0368e0e3c7be06914df5c78258a99b57b18519f5
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-07-02 18:13:59 -0400

    added update_holdings function

--- entity.py
+++ entity.py
@@ -8,8 +8,14 @@ class Entity:
     def get_holding(self, name):
         return self.holdings.get(name, 0)
     
+    def update_holdings(self, name, number):
+        self.holdings[name] = self.get_holding(name) + number
+
     def get_capital(self):
         return self.get_holding("capital")
     
+    def add_capital(self, number):
+        self.update_holdings("capital", number)
+
     def tick(self):
         pass
