83fa1aaa9ae29b9924b875df6c6a6100ee4969a6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-07-03 13:21:54 -0400

    added noise_generator args support

--- company.py
+++ company.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 from entity import Entity
+from util import CombineNoise, NoiseGenerator, create_price_generator
 
 class Company(Entity):
 
     def __init__(self, symbol, noise_generator):
         super().__init__()
         self.symbol = symbol
-        self.noise_generator = noise_generator
+        if isinstance(noise_generator, (CombineNoise, NoiseGenerator)):
+            self.noise_generator = noise_generator
+        else:
+            self.noise_generator = create_price_generator(**noise_generator)
         self.num_shares = 0
 
     def release_shares(self, num_shares_to_release):
