4c04c7be6a2d589b0af2ce909f20df642bba4728
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2023-07-04 09:47:05 -0400

    added multi symbol order book

--- order_book.py
+++ order_book.py
@@ -17,10 +17,12 @@ class OrderBook:
         for buy_sender, (buy_price, buy_quantity) in self.buy_orders.items():
             for sell_sender, (sell_price, sell_quantity) in self.sell_orders.items():
                 if buy_price >= sell_price: 
-                    trade_quantity = min(buy_quantity, sell_quantity)
+                    trade_quantity = min(buy_quantity, sell_quantity, sell_sender.get_holdings(self.symbol))
                     matched_trades.append((buy_sender, sell_sender, trade_quantity, sell_price))
                     buy_quantity -= trade_quantity
                     sell_quantity -= trade_quantity
+                    buy_sender.update_holdings(self.symbol, trade_quantity)
+                    sell_sender.update_holdings(self.symbol, -trade_quantity)
 
                     if buy_quantity == 0:
                         del self.buy_orders[buy_sender]
@@ -35,6 +37,31 @@ class OrderBook:
                     if buy_quantity == 0:
                         break
 
+        return matched_trades
+
+    def tick(self):
+        return self.match_orders()
+            
+class MultiSymbolOrderBook:
+
+    def __init__(self) -> None:
+        self.order_books = {}
+
+    def get_order_book(self, symbol):
+        if symbol not in self.order_books:
+            self.order_books[symbol] = OrderBook(symbol)
+        return self.order_books[symbol]
+    
+    def at(self, symbol, price, sender, quantity=1):
+        self.get_order_book(symbol).at(price, sender, quantity)
+    
+    def bid(self, symbol, price, sender, quantity=1):
+        self.get_order_book(symbol).bid(price, sender, quantity)
+
+    def match_orders(self):
+        matched_trades = {}
+        for symbol, order_book in self.order_books.items():
+            matched_trades[symbol] = order_book.match_orders()
         return matched_trades
 
     def tick(self):
